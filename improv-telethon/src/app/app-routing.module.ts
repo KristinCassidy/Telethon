f8b6ee163c9045870860ce428469fa1e56492b83
Kristin Cassidy <kristincassidy@Kristins-MacBook-Pro.local>
2020-11-24 04:24:50 -0600

    schedule page

--- improv-telethon/src/app/app-routing.module.ts
+++ improv-telethon/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomePageComponent } from './home-page/home-page.component';
+import { ClassSignUpComponent } from './class-sign-up/class-sign-up.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { SponsorsComponent } from './sponsors/sponsors.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'class-sign-up', component: ClassSignUpComponent},
+  {path: 'schedule', component: ScheduleComponent},
+  {path: 'sponsors', component: SponsorsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- improv-telethon/src/app/app.module.ts
+++ improv-telethon/src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ClassSignUpComponent } from './class-sign-up/class-sign-up.component';
 import { ScheduleComponent } from './schedule/schedule.component';
+import { SponsorsComponent } from './sponsors/sponsors.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ScholarshipComponent } from './scholarship/scholarship.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     ClassSignUpComponent,
-    ScheduleComponent
+    ScheduleComponent,
+    SponsorsComponent,
+    NavBarComponent,
+    ScholarshipComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
